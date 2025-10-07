bf80fbb878562ac3b89ce8fe6106d8048d868ca8
shahidkt <shahid.kt@ultsglobal.com>
2025-10-07 13:07:39 +0530

    [ADD] capstone_sale_price_calculation: Added module for sale net rate calculation

---
+++ custom-addons/capstone_sale_price_calculation/models/product_template.py
@@ -0,0 +1,17 @@
+from odoo import api, fields, models
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    mrp = fields.Float(
+        string="MRP",
+        help="Maximum Retail Price of the product."
+    )
+    net_rate_based_on = fields.Selection(
+        [
+            ('net_rate', 'Net Rate'),
+            ('margin', 'Margin'),
+        ],
+        string="Net Rate Based On",
+        default='net_rate',
+    )
---
+++ custom-addons/capstone_sale_price_calculation/models/sale_order.py
@@ -0,0 +1,38 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+
+    def _get_pricelist_price(self):
+        self.ensure_one()
+
+        price = super()._get_pricelist_price()
+
+        customer = self.order_id.partner_id
+        product = self.product_id
+
+        margin = getattr(customer, "margin_percent", 0.0) or 0.0
+        discount = getattr(customer, "discount_percent", 0.0) or 0.0
+
+        if not customer or not product:
+            return price
+
+        if product.net_rate_based_on == "net_rate":
+            return price
+
+        elif product.net_rate_based_on == "margin":
+            mrp = product.mrp or 0.0
+            margin_price = mrp - (mrp * margin / 100.0)
+            final_price = margin_price - (margin_price * discount / 100.0)
+            return final_price
+
+        return price
+
+
+
+
+
+
