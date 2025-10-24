3d8953473d18a3cf030a3333ade1833008006b23
shahidkt <shahid.kt@ultsglobal.com>
2025-10-24 22:39:27 +0530

    [FIX] capstone_sales_return_custom: Sale Return fine tuning

--- custom-addons/capstone_sales_return_custom/models/sale_order.py
+++ custom-addons/capstone_sales_return_custom/models/sale_order.py
@@ -6,6 +6,51 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     is_discount_sale = fields.Boolean("Is Discount Sale")
+    sale_return_ids = fields.One2many(
+        'sale.return.request.line',
+        'sale_order_id'
+    )
+    sale_return_count = fields.Integer(
+        string='Sale Returns',
+        compute='_compute_sale_return_count'
+    )
+
+    def get_available_sale_qty(self, product_id,product_template_id):
+        """
+        Return available sale quantity for given product template.
+        available_qty = total ordered qty - total returned qty
+        """
+        self.ensure_one()
+
+        # 1️⃣ Total ordered qty from sale.order.line
+        order_lines = self.order_line.filtered(
+            lambda l: l.product_template_id.id == product_template_id
+        )
+        ordered_qty = sum(order_lines.mapped('product_uom_qty'))
+
+        # 2️⃣ Total returned qty from sale_return_ids
+        return_lines = self.sale_return_ids.filtered(
+            lambda r: r.product_id.id == product_id
+        )
+        returned_qty = sum(return_lines.mapped('return_qty'))
+
+        # 3️⃣ Calculate available sale quantity
+        available_qty = ordered_qty - returned_qty
+
+        return available_qty
+
+
+    def _compute_sale_return_count(self):
+        for order in self:
+            order.sale_return_count = len(order.sale_return_ids)
+
+    def action_view_sale_returns(self):
+        """Open sale return lines related to this order"""
+        self.ensure_one()
+        action = self.env.ref('capstone_sales_return_custom.action_sale_return_request_line').read()[0]
+        action['domain'] = [('sale_order_id', '=', self.id)]
+        action['context'] = {'default_sale_order_id': self.id}
+        return action
 
     def action_confirm(self):
         """Override action_confirm to handle discount sale locations"""
